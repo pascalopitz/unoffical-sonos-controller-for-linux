d37881079dcd7c9ac0cbd340caec201ae84b2a5c
Pascal Opitz <contact@pascalopitz.com>
2015-02-05 22:55:07 +1030

    using react draggable for volume slider (not setting volume yet)

--- src/ui/components/VolumeSlider.jsx
+++ src/ui/components/VolumeSlider.jsx
@@ -1,25 +1,42 @@
 const width = 180;
 
 import React from 'react/addons';
+import Draggable from 'react-draggable2';
 import { Cursor, ImmutableOptimizations }  from 'react-cursor';
 import EventableMixin from '../mixins/EventableMixin';
 
 class VolumeSlider {
 
+	getInitialState() {
+		return { dragging: false };
+	}
+
 	render () {
 		var id = this.props.id || '';
 
-		var margin = width / 100 * this.props.volume.value;
+		var left = width / 100 * this.props.volume.value;
 
-		var styles = {
-			marginLeft: margin + "px",			
-		};
+		var pos = { x: left, y: 0 };
 
 		return (
-			<div id={this.props.id} className="volume-bar">
-				<img style={styles}  src="images/popover_vol_scrubber_normal.png" />
+			<div
+				id={this.props.id}
+				className="volume-bar">
+				<Draggable
+					axis="x"
+					handle="img"
+					onDrag={this._onDrag}
+					start={pos}
+					bound="all box">
+					<img
+						src="images/popover_vol_scrubber_normal.png" />
+				</Draggable>
 			</div>
 		);
+	}
+
+	_onDrag (e)  {
+		console.log(arguments);
 	}
 }
 
