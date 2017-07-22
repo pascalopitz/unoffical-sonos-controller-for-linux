4cd022793421930e6e0e54f0e8cee497e6732862
Pascal Opitz <contact@pascalopitz.com>
2017-07-22 23:35:33 +0930

    Bumped version, fixed formatting in main.js, added menu items

--- app/main.js
+++ app/main.js
@@ -1,129 +1,142 @@
 const electron = require('electron');
 const { app, Menu, BrowserWindow } = electron;
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
 const wakeEvent = require('wake-event');
 
-
 let win;
 
-function createWindow () {
+function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 600 });
 
-	win = new BrowserWindow({width: 800, height: 600})
+    const menu = Menu.buildFromTemplate([
+        {
+            label: 'View',
+            submenu: [
+                {
+                    role: 'quit'
+                },
+                {
+                    role: 'close'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'resetzoom'
+                },
+                {
+                    role: 'zoomin'
+                },
+                {
+                    role: 'zoomout'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'togglefullscreen'
+                }
+            ]
+        },
+        {
+            label: 'Developer',
+            submenu: [
+                {
+                    label: 'Reload',
+                    accelerator: 'CmdOrCtrl+R',
+                    click(item, focusedWindow) {
+                        if (focusedWindow) {
+                            focusedWindow.reload();
+                        }
+                    }
+                },
+                {
+                    label: 'Toggle Developer Tools',
+                    accelerator: 'Ctrl+Shift+I',
+                    click(item, focusedWindow) {
+                        if (focusedWindow) {
+                            focusedWindow.webContents.toggleDevTools();
+                        }
+                    }
+                }
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Project page',
+                    click() {
+                        electron.shell.openExternal(
+                            'https://github.com/pascalopitz/unoffical-sonos-controller-for-linux'
+                        );
+                    }
+                },
+                {
+                    label: 'Report an Issue',
+                    click() {
+                        electron.shell.openExternal(
+                            'https://github.com/pascalopitz/unoffical-sonos-controller-for-linux/issues'
+                        );
+                    }
+                },
+                {
+                    label: 'Latest Releases',
+                    click() {
+                        electron.shell.openExternal(
+                            'https://github.com/pascalopitz/unoffical-sonos-controller-for-linux/releases'
+                        );
+                    }
+                }
+            ]
+        }
+    ]);
 
-	const menu = Menu.buildFromTemplate([
-		{
-			label: 'View',
-			submenu: [
-				{
-					label: 'Reload',
-					accelerator: 'CmdOrCtrl+R',
-					click (item, focusedWindow) {
-						if (focusedWindow) {
-							focusedWindow.reload();
-						}
-					}
-				},
-				{
-					label: 'Toggle Developer Tools',
-					accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
-					click (item, focusedWindow) {
-						if (focusedWindow) {
-							focusedWindow.webContents.toggleDevTools();
-						}
-					}
-				},
-				{
-					type: 'separator'
-				},
-				{
-					role: 'resetzoom'
-				},
-				{
-					role: 'zoomin'
-				},
-				{
-					role: 'zoomout'
-				},
-				{
-					type: 'separator'
-				},
-				{
-					role: 'togglefullscreen'
-				}
-			]
-		},
-		{
-			role: 'help',
-			submenu: [
-				{
-					label: 'Project page',
-					click () {
-						electron.shell.openExternal('https://github.com/pascalopitz/unoffical-sonos-controller-for-linux')
-					}
-				},
-				{
-					label: 'Report an Issue',
-					click () {
-						electron.shell.openExternal('https://github.com/pascalopitz/unoffical-sonos-controller-for-linux/issues')
-					}
-				},
-				{
-					label: 'Latest Releases',
-					click () {
-						electron.shell.openExternal('https://github.com/pascalopitz/unoffical-sonos-controller-for-linux/releases')
-					}
-				},
-			]
-		}
-	]);
+    Menu.setApplicationMenu(menu);
 
-	Menu.setApplicationMenu(menu);
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, 'window.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    );
 
-	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'window.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
+    win.on('closed', () => {
+        win = null;
+    });
 
-	win.on('closed', () => {
-		win = null;
-	});
-
-	wakeEvent(() => {
-		if (win) {
-			win.reload();
-		}
-	});
-
+    wakeEvent(() => {
+        if (win) {
+            win.reload();
+        }
+    });
 }
 
 const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
-	// Someone tried to run a second instance, we should focus our window.
-	if (win) {
-		if (win.isMinimized()) {
-			win.restore();
-		}
-		win.focus();
-	}
-})
+    // Someone tried to run a second instance, we should focus our window.
+    if (win) {
+        if (win.isMinimized()) {
+            win.restore();
+        }
+        win.focus();
+    }
+});
 
 if (shouldQuit) {
-	app.quit();
+    app.quit();
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+    app.quit();
 });
 
 app.on('activate', () => {
-	if (win === null) {
-		createWindow()
-	}
+    if (win === null) {
+        createWindow();
+    }
 });
-
