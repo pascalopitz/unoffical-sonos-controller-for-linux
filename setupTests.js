7101f3b0a04b4b940aca7130ac6aac845ecc06f7
Pascal Opitz <contact@pascalopitz.com>
2021-12-19 14:36:40 +1030

    fix tests

--- setupTests.js
+++ setupTests.js
@@ -1,3 +1,8 @@
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 configure({ adapter: new Adapter() });
+
+window.QueueActions = {};
+window.ZoneGroupActions = {};
+window.BrowserListSelectors = {};
+window.BrowserListActions = {};
